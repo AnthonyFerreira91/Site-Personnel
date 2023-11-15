5af8d7d4dcf2ae4d7b5ce25207538c44682065f2
Anthony Ferreira <matrixfrpro71230@hotmail.fr>
2023-11-15 11:30:06 +0100

    Correction taille description ProjectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,8 +15,8 @@ export const ProjectCard = ({ projetUrl, imgUrl, imgAlt, title, description, too
     <Link href={projetUrl} target="_blank" className="bg-primary-hover transition-all hover:scale-105 min-w-[384px] min-h-[320px] h-80 w-96 p-0.5 rounded-[14px]">
       <div className="relative bg-dark dark:shadow-gray-800 hover:shadow-none shadow-md shadow-blue-100 cursor-pointer pb-2 bg-white rounded-xl w-full h-full">
         <Image src={imgUrl} alt={imgAlt} width={384} height={160} className="rounded-t-xl" />
-        <div className="flex flex-col justify-between">
-          <h3 className="text-center font-semibold">{title}</h3>
+        <div className="flex flex-col gap-3 h-[100px]">
+          <h3 className="text-center font-semibold mt-1">{title}</h3>
           <p className="text-center">{description}</p>
         </div>
         <div className="absolute bottom-0 min-w-[384px] w-[384px] flex justify-evenly">
