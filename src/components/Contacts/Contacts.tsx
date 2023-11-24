8c8a28ff945d013e5f37d674876eefae957a9236
Anthony Ferreira <matrixfrpro71230@hotmail.fr>
2023-11-24 10:11:02 +0100

    Good link GitHub

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export const Contacts = () => {
 	return (
 		<div className='pt-1'>
-			<Link href="https://github.com/Matrixfrpro33" className="bg-primary hover:scale-105 inline-flex gap-1.5 text-white items-center justify-center text-sm font-medium transition-all h-9 rounded-md px-3 mr-2" target='_blank'>
+			<Link href="https://github.com/AnthonyFerreira91" className="bg-primary hover:scale-105 inline-flex gap-1.5 text-white items-center justify-center text-sm font-medium transition-all h-9 rounded-md px-3 mr-2" target='_blank'>
 				<SiGithub /> GitHub
 			</Link>
 			<Link href="https://www.linkedin.com/in/anthony-ferreira-512086181/" className="bg-primary hover:scale-105 inline-flex gap-1.5 text-white items-center justify-center text-sm font-medium transition-all h-9 rounded-md px-3 mr-2" target='_blank'>
